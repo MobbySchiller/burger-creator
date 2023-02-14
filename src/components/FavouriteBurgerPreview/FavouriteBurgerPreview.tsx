2f82256ee4cc89e72e510de2200421d729650e76
MobbySchiller <kb.burzynski@gmail.com>
2023-02-14 15:46:45 +0100

    refactor: back to spaces between ingredients in preview

--- src/components/FavouriteBurgerPreview/FavouriteBurgerPreview.tsx
+++ src/components/FavouriteBurgerPreview/FavouriteBurgerPreview.tsx
@@ -10,12 +10,8 @@ const FavouriteBurgerPreview: FC<FavouriteBurgerPreviewProps> = ({ ingredients }
     const favouriteBurgerIngredients = ingredients.map((ingredient, index) => (
         <div
             key={`${ingredient}-${index}`}
-            className={`favourite-burger__${ingredient}`}
-            style={{
-                transform: `translateY(-${index}0px)`,
-                zIndex: `${100 - index}`
-            }}
-        ></div>
+            className={`favourite-burger__${ingredient}`}>
+        </div>
     ))
 
     return (
