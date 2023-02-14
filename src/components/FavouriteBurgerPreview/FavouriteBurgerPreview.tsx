530df743c988141e091011b0ba474bead59cd8ed
MobbySchiller <kb.burzynski@gmail.com>
2023-02-14 13:27:22 +0100

    feat: complete favourite burgers preview

---
+++ src/components/FavouriteBurgerPreview/FavouriteBurgerPreview.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react'
+import './FavouriteBurgerPreview.scss'
+
+type FavouriteBurgerPreviewProps = {
+    ingredients: string[]
+}
+
+const FavouriteBurgerPreview: FC<FavouriteBurgerPreviewProps> = ({ ingredients }) => {
+
+    const favouriteBurgerIngredients = ingredients.map((ingredient, index) => (
+        <div key={`${ingredient}-${index}`} className={`favourite-burger__${ingredient}`}></div>
+    ))
+
+    return (
+        <div className='favourite-burger'>
+            {favouriteBurgerIngredients}
+        </div>
+    )
+}
+
+export default FavouriteBurgerPreview
--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -1,18 +1,28 @@
-import { FC, useEffect, useContext } from 'react'
+import { FC, useContext } from 'react'
 import { FavouriteBurgersContext } from '../../context/FavouriteBurgersContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import Header from '../../components/Header/Header'
+import FavouriteBurgerPreview from '../../components/FavouriteBurgerPreview/FavouriteBurgerPreview'
 import './Favourites.scss'
 
 const Favourites: FC = () => {
     const { favouriteBurgers } = useContext(FavouriteBurgersContext)
 
-    // useEffect(() => {
-
-    // }, [favouriteBurgers])
-
     const burgersToDisplay = favouriteBurgers.map(burger => (
-        <div key={burger.name}>
-            <p>{burger.name}</p>
+        <div
+            key={burger.name}
+            className='burger-preview'
+        >
+            <FavouriteBurgerPreview ingredients={burger.ingredients} />
+            <p className='burger-preview__name'>{burger.name}</p>
+            <button className='burger-preview__button'>
+                <FontAwesomeIcon
+                    icon={faTrash}
+                    className='burger-preview__button-icon'
+                />
+                Remove
+            </button>
         </div>
     ))
 
@@ -23,7 +33,9 @@ const Favourites: FC = () => {
                 {!favouriteBurgers.length &&
                     <p className='favourites__message'>You don’t have any favourites burgers</p>
                 }
-                {burgersToDisplay}
+                <div className='favourites__burgers'>
+                    {burgersToDisplay}
+                </div>
             </main>
         </>
     )
