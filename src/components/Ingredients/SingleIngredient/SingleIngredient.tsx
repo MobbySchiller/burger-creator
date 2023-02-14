7524f91406618746e2730b9690ba10fb9c6914f9
MobbySchiller <kb.burzynski@gmail.com>
2023-02-14 01:08:16 +0100

    feat: add ingredients management conditions

--- src/components/Ingredients/SingleIngredient/SingleIngredient.tsx
+++ src/components/Ingredients/SingleIngredient/SingleIngredient.tsx
@@ -14,18 +14,32 @@ const SingleIngredient: FC<SingleIngredientProps> = ({ ingredient }) => {
     const { name, value, img } = ingredient
 
     const [currentBurger, dispatch] = useContext(CurrentBurgerContext)
+    const { ingredients } = currentBurger
 
-    const currentIngredientAmount = currentBurger.ingredients
+    const currentIngredientAmount = ingredients
         .filter(ingredient => ingredient === value)
         .length
 
     const handleRemove = () => {
         if (currentIngredientAmount === 0) return
+        if (ingredients.length > 1 && value === 'bottomBun') return
         dispatch({ type: 'remove', payload: value })
     }
 
     const handleAdd = () => {
+        if (isTopBunInBurger()) return
+        if (ingredients.length === 8 && value !== 'topBun') return
+        if (isBottomBunInBurger() && value === 'bottomBun') return
+        if (ingredients.length === 0 && value !== 'bottomBun') return
         dispatch({ type: 'add', payload: value })
+    }
+
+    const isTopBunInBurger = () => {
+        return ingredients.some(ingredient => ingredient === 'topBun')
+    }
+
+    const isBottomBunInBurger = () => {
+        return ingredients.some(ingredient => ingredient === 'bottomBun')
     }
 
     return (
