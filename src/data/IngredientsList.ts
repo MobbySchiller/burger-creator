78c48cb2137bda843812f52a89bd27b6809f5cc5
MobbySchiller <kb.burzynski@gmail.com>
2023-02-14 18:07:36 +0100

    fix: replacing broken pictures, back to the previous images structure and references

--- src/data/IngredientsList.ts
+++ src/data/IngredientsList.ts
@@ -1,11 +1,11 @@
 import { IngredientList } from '../types/Ingredients'
-import bottomBunImg from '../assets/pictures/bottom-bun.png'
-import topBunImg from '../assets/pictures/top-bun.png'
-import tomatoImg from '../assets/pictures/tomato.png'
-import burgerImg from '../assets/pictures/ingredient-burger.png'
-import fishImg from '../assets/pictures/ingredient-fish.png'
-import lettuceImg from '../assets/pictures/lettuce.png'
-import cheeseImg from '../assets/pictures/ingredient-cheese.png'
+import bottomBunImg from '../assets/bottom-bun.png'
+import topBunImg from '../assets/top-bun.png'
+import tomatoImg from '../assets/tomato.png'
+import burgerImg from '../assets/ingredient-burger.png'
+import fishImg from '../assets/ingredient-fish.png'
+import lettuceImg from '../assets/lettuce.png'
+import cheeseImg from '../assets/ingredient-cheese.png'
 
 const IngredientsList: IngredientList[] = [
     {
